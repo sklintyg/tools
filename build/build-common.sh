adb492fbef89f1c386388ca208eb1349187322ba
Magnus Ekstrand <magnus.ekstrand@callistaenterprise.se>
2014-10-31 23:56:44 +0100

    Lade till bygge för comon-support

--- build/build-common.sh
+++ build/build-common.sh
@@ -4,6 +4,8 @@ export INTYG_HOME=`pwd`/../..
 
 cd $INTYG_HOME/common/pom
 mvn install
+cd $INTYG_HOME/common/support
+mvn install
 cd $INTYG_HOME/common/web
 mvn install
 cd $INTYG_HOME/common/util/logging-util
