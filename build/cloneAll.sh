6d5b96772c62a45b9e8cec4e95a3668b4083bfa7
Magnus Ekstrand <magnus@ekstrand.net>
2015-04-01 11:51:22 +0200

    Lade till clone för common-pom

--- build/cloneAll.sh
+++ build/cloneAll.sh
@@ -3,6 +3,9 @@
 export INTYG_HOME=`pwd`/../..
 
 cd $INTYG_HOME
+git clone https://github.com/sklintyg/common-pom.git
+if [ $? != 0 ]; then exit 1; fi
+
 git clone https://github.com/sklintyg/common.git
 if [ $? != 0 ]; then exit 1; fi
 
