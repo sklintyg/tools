64a7f8fb9ead67f95fd9e376616a2a3df2a0b4c8
stephenwhite <stephen.white@callistaenterprise.se>
2015-04-01 10:49:36 +0200

    update clone had a few probs

--- build/cloneAll.sh
+++ build/cloneAll.sh
@@ -2,26 +2,21 @@
 
 export INTYG_HOME=`pwd`/../..
 
-git clone https://github.com/sklintyg/common.git
 cd $INTYG_HOME
+git clone https://github.com/sklintyg/common.git
 if [ $? != 0 ]; then exit 1; fi
 
 git clone https://github.com/sklintyg/intygstyper.git
-cd $INTYG_HOME
 if [ $? != 0 ]; then exit; fi
 
 git clone https://github.com/sklintyg/intygstjanst.git
-cd $INTYG_HOME
 if [ $? != 0 ]; then exit; fi
 
 git clone https://github.com/sklintyg/minaintyg.git
-cd $INTYG_HOME
 if [ $? != 0 ]; then exit; fi
 
 git clone https://github.com/sklintyg/schemas.git
-cd $INTYG_HOME
 if [ $? != 0 ]; then exit; fi
 
 git clone https://github.com/sklintyg/webcert.git
-cd $INTYG_HOME
 if [ $? != 0 ]; then exit; fi
