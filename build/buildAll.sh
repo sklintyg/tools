28a32fea8d99ded7a99c4393b82e5d0b8c91855c
Benny Samuelsson <benny.samuelsson@usify.se>
2015-10-14 13:52:31 +0200

    Rätt skript ska skippa testerna!

--- build/buildAll.sh
+++ build/buildAll.sh
@@ -7,29 +7,29 @@ mvn clean install -Ddependency.unpack-skip=true
 if [ $? != 0 ]; then exit 1; fi
 
 cd $INTYG_HOME/schemas
-mvn clean install -DskipTests=true
+mvn clean install
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/common
-mvn clean install -DskipTests=true
+mvn clean install
 if [ $? != 0 ]; then exit 1; fi
 
 cd $INTYG_HOME/tools
-mvn clean install -DskipTests=true
+mvn clean install
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstyper
-mvn clean install -DskipTests=true
+mvn clean install
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstjanst
-mvn clean install -DskipTests=true
+mvn clean install
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/minaintyg
-mvn clean install -DskipTests=true
+mvn clean install
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/webcert
-mvn clean install -DskipTests=true
+mvn clean install
 if [ $? != 0 ]; then exit; fi
--- build/buildAllSkipTests.sh
+++ build/buildAllSkipTests.sh
@@ -7,29 +7,29 @@ mvn clean install -Ddependency.unpack-skip=true
 if [ $? != 0 ]; then exit 1; fi
 
 cd $INTYG_HOME/schemas
-mvn clean install
+mvn clean install -DskipTests=true
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/common
-mvn clean install
+mvn clean install -DskipTests=true
 if [ $? != 0 ]; then exit 1; fi
 
 cd $INTYG_HOME/tools
-mvn clean install
+mvn clean install -DskipTests=true
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstyper
-mvn clean install
+mvn clean install -DskipTests=true
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstjanst
-mvn clean install
+mvn clean install -DskipTests=true
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/minaintyg
-mvn clean install
+mvn clean install -DskipTests=true
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/webcert
-mvn clean install
+mvn clean install -DskipTests=true
 if [ $? != 0 ]; then exit; fi
