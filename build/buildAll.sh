3f5f9d8b11c75f91d170ce2eb176ea996cef0888
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-10-20 14:32:42 +0200

    DEVENV: Går nu att skicka med flaggor till maven

--- build/buildAll.sh
+++ build/buildAll.sh
@@ -3,33 +3,33 @@
 export INTYG_HOME=`pwd`/../..
 
 cd $INTYG_HOME/common-pom
-mvn clean install -Ddependency.unpack-skip=true
+mvn clean install -Ddependency.unpack-skip=true $@
 if [ $? != 0 ]; then exit 1; fi
 
 cd $INTYG_HOME/schemas
-mvn clean install checkstyle:check
+mvn clean install checkstyle:check $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/common
-mvn clean install checkstyle:check
+mvn clean install checkstyle:check $@
 if [ $? != 0 ]; then exit 1; fi
 
 cd $INTYG_HOME/tools
-mvn clean install checkstyle:check
+mvn clean install checkstyle:check $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstyper
-mvn clean install checkstyle:check
+mvn clean install checkstyle:check $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstjanst
-mvn clean install checkstyle:check
+mvn clean install checkstyle:check $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/minaintyg
-mvn clean install checkstyle:check
+mvn clean install checkstyle:check $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/webcert
-mvn clean install checkstyle:check
+mvn clean install checkstyle:check $@
 if [ $? != 0 ]; then exit; fi
