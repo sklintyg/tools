b9aa0351ca55dbd18f395dc18cbd9e373313d5cc
Magnus Ekstrand <magnus@ekstrand.net>
2015-11-12 22:29:12 +0100

    BUILDENV: Lade till val för att använda checkstyle:check eller inte. Default är y (yes)

--- build/buildAll.sh
+++ build/buildAll.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+CMD="mvn clean install checkstyle:check"
+DEFAULT="y"
+read -e -p "Use checkstyle:check [y/n]? [y]: " PROCEED
+# adopt the default, if 'enter' given
+PROCEED="${PROCEED:-${DEFAULT}}"
+# change to lower case to simplify following if
+PROCEED="${PROCEED,,}"
+
+# condition for specific letter
+if [ "${PROCEED}" == "n" ] ; then
+  CMD="mvn clean install" 
+fi
+
 export INTYG_HOME=`pwd`/../..
+
 
 cd $INTYG_HOME/common-pom
 mvn clean install -Ddependency.unpack-skip=true $@
@@ -11,25 +25,25 @@ mvn clean install $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/common
-mvn clean install checkstyle:check $@
+${CMD} $@
 if [ $? != 0 ]; then exit 1; fi
 
 cd $INTYG_HOME/tools
-mvn clean install checkstyle:check $@
+${CMD} $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstyper
-mvn clean install checkstyle:check $@
+${CMD} $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/intygstjanst
-mvn clean install checkstyle:check $@
+${CMD} $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/minaintyg
-mvn clean install checkstyle:check $@
+${CMD} $@
 if [ $? != 0 ]; then exit; fi
 
 cd $INTYG_HOME/webcert
-mvn clean install checkstyle:check $@
+${CMD} $@
 if [ $? != 0 ]; then exit; fi
