5c6a035bf0e31c01e0bf7dd6413686857bc389de
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-09-23 12:52:11 +0200

    DEVEND: Lagt till lite timing info i buildAll.sh

--- buildTools/buildAll.sh
+++ buildTools/buildAll.sh
@@ -8,7 +8,12 @@ read -e -p "Use checkstyle:check [y/n]? [y]: " PROCEED
 
 INTYG_HOME="$( cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd )"
 
+start_time=`date +%s`
+
 for project in common intygstyper intygstjanst minaintyg webcert; do
     cd "$INTYG_HOME/$project"
     ${CMD} "$@" || exit 1
 done
+
+duration=$(expr `date +%s` - $start_time)
+echo Build completed at `date +%T` after a total of  $(($duration / 60)) min $(($duration % 60)) sec
