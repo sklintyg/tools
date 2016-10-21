58db9cdb58043a3bb60da8545c88cb54aed9f117
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2016-10-21 14:12:39 +0200

    DEVENV: lägg till nytt byggscript som skippar både tester och clean

---
+++ buildTools/buildAllSkipTestsNoClean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INTYG_HOME="$( cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd )"
+
+start_time=$(date +%s)
+
+for project in common intygstyper intygstjanst minaintyg webcert; do
+    cd "$INTYG_HOME/$project"
+    ./gradlew --parallel assemble install || exit 1
+done
+
+duration=$(( $(date +%s) - $start_time ))
+echo
+echo Build using script $(basename "$0") completed at $(date +%T) after a total of $(($duration / 60)) min $(($duration % 60)) sec.
