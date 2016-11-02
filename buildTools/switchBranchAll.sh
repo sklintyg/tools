3d7ea517cbac8972b7089b803c3bc9ed08165617
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2016-11-02 07:35:14 +0100

    DEVENV: mer användbart switch-script

--- buildTools/switchBranchAll.sh
+++ buildTools/switchBranchAll.sh
@@ -2,9 +2,11 @@
 
 INTYG_HOME="$( cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd )"
 
+branch=${1:-develop}
+
 for project in schemas common intygstyper intygstjanst minaintyg webcert; do
     echo $project
     cd "$INTYG_HOME/$project"
-    git checkout develop
+    git checkout $branch
     echo
 done
