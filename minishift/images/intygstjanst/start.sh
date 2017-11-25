e1534c252c18712b36561ab6b8df1c7ef4baf53c
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-11-25 15:02:42 +0100

    DEVENV: Replaced liquibase username/password with ENV vars

--- minishift/images/intygstjanst/start.sh
+++ minishift/images/intygstjanst/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /liquibase
-bin/intygstjanst-liquibase-runner --url=jdbc:mysql://mysql:3306/intyg?useCompression=true --username=intyg --password=intyg update;
+bin/intygstjanst-liquibase-runner --url=jdbc:mysql://mysql:3306/intyg?useCompression=true --username=$INTYG_DB_USERNAME --password=$INTYG_DB_PASSWORD update;
 cd /usr/local/tomcat
 catalina.sh run
