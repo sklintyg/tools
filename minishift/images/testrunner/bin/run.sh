37cf8bc1ff481083ffcbfce4462521507e42d20b
Erik Forsgren <erik.forsgren@r2m.se>
2018-05-21 09:07:32 +0200

    Update run.sh

--- minishift/images/testrunner/bin/run.sh
+++ minishift/images/testrunner/bin/run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 git clone -v $GIT_URL repo
-
-ls /usr/lib/jvm
+ls -lah repo
 
 # Execute
-repo/gradlew assemble restAssuredTest -DbaseUrl=$TARGET_URL -DbuildVersion=$BUILD_VERSION -DcommonVersion=$COMMON_VERSION -DinfraVersion=$INFRA_VERSION
+cd repo && ./gradlew assemble restAssuredTest -DbaseUrl=$TARGET_URL -DbuildVersion=$BUILD_VERSION -DcommonVersion=$COMMON_VERSION -DinfraVersion=$INFRA_VERSION
 
 # Copy test results to persistent volume mount
 #cp -r repo/web/build/reports/tests/restAssuredTest/* /tmp/reports/$JOB_NAME/$BUILD_VERSION/
