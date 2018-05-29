d23bc12f6732d44d153e8358daf5e59e0c01e518
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-05-29 09:33:36 +0200

    INTYG-6361: Tagit bort referenser till templates som inte finns

--- minishift/bootstrap-env.sh
+++ minishift/bootstrap-env.sh
@@ -6,9 +6,3 @@ oc project labbtest
 # oc adm policy add-scc-to-user anyuid -z default
 oc policy add-role-to-group     system:image-puller system:serviceaccounts:labbtest     --namespace=intygstjanster-test
 
-oc create -R -f templates/mysql
-oc create -R -f templates/activemq
-#oc create -R -f templates/logsender/deploy
-#oc create -R -f templates/intygstjanst/deploy
-#oc create -R -f templates/webcert/deploy
-
