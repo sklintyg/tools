845a711a12beb1123a90c596da9599e00fa15699
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-06-15 12:04:04 +0200

    DEVENV: KEEP added

--- devops/openshift/clean-istags.sh
+++ devops/openshift/clean-istags.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Cleanup image stream tags, only keep a maximum number
+
+KEEP=10
 function clean() {
 	name=$1
 	tags=($(echo $(oc get is $name --template='{{range .status.tags}}{{.tag}}{{"\n"}}{{end}}')))
 	n_tags=${#tags[@]}
-	if [ $n_tags -gt 10 ]; then
-		n_rem=$((${n_tags} - 10))
+	if [ ${n_tags} -gt ${KEEP} ]; then
+		n_rem=$((${n_tags} - ${KEEP}))
 		for tag in ${tags[*]}
 		do
 			oc delete istag/${name}:${tag}
