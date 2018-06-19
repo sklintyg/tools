49e7fee8b3391e615cac593f7890de486f38e99c
Carl Frendin <carl.frendin@inera.se>
2018-06-19 10:38:22 +0200

    INTYG-6420: Mindre förbättringar av datadumpningsscript för gdpr för PP

--- gdpr-dump/gdpr_pp_part1_pp.sh
+++ gdpr-dump/gdpr_pp_part1_pp.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+####################
+# Usage: ./gdpr_pp_part1_pp.sh <database_user> <database_password> <pp_database_name>
+# Två textfiler med databasdumpar skapas i current working directory, pp_gdpr_query_result.txt och pp_allavardgivare_dump.txt
+# Dessa används av nästa script, gdpr_pp_part2_it.sh
+####################
 
 USER=$1
 PASSWORD=$2
+# In development, we use 'privatlakarportal'
+PRIVATLAKARPORTAL_DATABASE_NAME=$3
 
 QUERY="select VARDGIVARE_ID,PERSONID,VARDGIVARE_NAMN,EPOST from PRIVATLAKARE order by VARDGIVARE_ID;"
 echo "Executing query: $QUERY"
-PRIVATLAKARPORTAL_QUERY_RESULT="$(mysql --user=$USER --password=$PASSWORD --batch -e "use privatlakarportal; $QUERY")"
+PRIVATLAKARPORTAL_QUERY_RESULT="$(mysql --user=$USER --password=$PASSWORD --batch -e "use $PRIVATLAKARPORTAL_DATABASE_NAME; $QUERY")"
+# For use in final result
 echo "$PRIVATLAKARPORTAL_QUERY_RESULT" > pp_gdpr_query_result.txt
 
-QUERY="select VARDGIVARE_ID from PRIVATLAKARE order by VARDGIVARE_ID;"
-echo "Executing query: $QUERY"
-VARDGIVARE_I_PP="$(mysql --user=$USER --password=$PASSWORD --batch -e "use privatlakarportal; $QUERY" | tail -n +2)"
 # Skapa en lista av samtliga privatläkare
+VARDGIVARE_I_PP="$(echo "$PRIVATLAKARPORTAL_QUERY_RESULT" | cut -f 1 | tail -n +2)"
 echo "$VARDGIVARE_I_PP" > pp_allavardgivare_dump.txt
 
 
--- gdpr-dump/gdpr_pp_part2_it.sh
+++ gdpr-dump/gdpr_pp_part2_it.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+####################
+# Usage: ./gdpr_pp_part2_it.sh <database_user> <database_password> <it_database_name>
+# Två textfiler med databasdumpar från privatläkarportalen läses in, pp_gdpr_query_result.txt och pp_allavardgivare_dump.txt
+# Dessa måste ligga i working directory vid körning. Efter att scriptet körs så läggs resultatet i pp_gdpr_dump_result.txt i working directory.
+####################
 
 USER=$1
 PASSWORD=$2
+# 'intygstjanst' is used in development.
+INTYGSTJANST_DATABASE_NAME=$3
 
 PRIVATLAKARPORTAL_QUERY_RESULT="$(cat pp_gdpr_query_result.txt)"
 VARDGIVARE_I_PP="$(cat pp_allavardgivare_dump.txt)"
@@ -14,14 +21,14 @@ where $(echo "$VARDGIVARE_I_PP" | sed -r 's/(.+)/CARE_GIVER_ID=\x27\1\x27 or /' 
 group by CERTIFICATE.CARE_GIVER_ID order by CERTIFICATE.CARE_GIVER_ID;"
 
 echo "Executing query: $QUERY"
-INTYGSTJANST_QUERY_RESULT="$(mysql --user=$USER --password=$PASSWORD --batch -e "use intygstjanst; $QUERY")"
+INTYGSTJANST_QUERY_RESULT="$(mysql --user=$USER --password=$PASSWORD --batch -e "use $INTYGSTJANST_DATABASE_NAME; $QUERY")"
 
 #Time for mixing the results
-CLEANED_MAIN_DATA="$(echo "$PRIVATLAKARPORTAL_QUERY_RESULT" | sort -k 1)"
+CLEANED_MAIN_DATA="$(echo "$PRIVATLAKARPORTAL_QUERY_RESULT" | grep -F "${VARDGIVARE_I_PP}" | sort -k 1)"
 CLEANED_AUGMENT_DATA="$(echo "${INTYGSTJANST_QUERY_RESULT}" | grep -F "${VARDGIVARE_I_PP}" | sort -k 1)"
 
-echo -e "VARDGIVARE_ID\tPERSONID\tVARDGIVARE_NAMN\tEPOST\tantal_intyg\tsenaste_intyg_inkommet_datum"
+echo -e "VARDGIVARE_ID\tPERSONID\tVARDGIVARE_NAMN\tEPOST\tantal_intyg\tsenaste_intyg_inkommet_datum" > pp_gdpr_dump_result.txt
 #outer left join, on VARDGIVARE_ID
-echo "$(join -t $'\t' -a 1 -1 1 -2 1 <(echo "$CLEANED_MAIN_DATA") <(echo "$CLEANED_AUGMENT_DATA"))" > pp_gdpr_dump.txt
+echo "$(join -t $'\t' -a 1 -1 1 -2 1 <(echo "$CLEANED_MAIN_DATA") <(echo "$CLEANED_AUGMENT_DATA"))" >> pp_gdpr_dump_result.txt
 
 
