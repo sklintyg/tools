d3b185762d1ae70b6ebbaf295d0bed4859475929
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-11-12 15:13:18 +0100

    DEVENV: Another fix for linux date

--- devops/openshift/clean-pods.sh
+++ devops/openshift/clean-pods.sh
@@ -4,18 +4,30 @@
 
 # Old is 1 day (24H)
 OLD=1
-case "$(uname)" in
+OS=$(uname)
+case "$OS" in
     "Linux")
         BEFORE=$(date --date "${OLD} days ago" +%s)
         ;;
     "Darwin")
         BEFORE=$(date -v-${OLD}d +%s)
         ;;
+    *)
+        echo "$OS: Unsupported platform (date function)"
+        exit 1
+        ;;
 esac
 
 function clean() {
     name=$1
-    time=$(date -j -f%Y-%m-%dT%H:%M:%SZ "$2" +%s)
+    case "$OS" in
+        "Linux")
+            time=$(date --date "$2" +%s)
+            ;;
+        "Darwin")
+            time=$(date -j -f%Y-%m-%dT%H:%M:%SZ "$2" +%s)
+            ;;
+    esac
     if [ $time -lt $BEFORE ]; then
         oc delete pod "$name"
     fi
