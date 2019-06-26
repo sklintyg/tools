9bf3ee5d22f7fd1c3f144d549ef73100d0b9b7ee
Magnus Ekstrand <magnus@ekstrand.net>
2019-06-26 10:43:54 +0200

    BUILDENV: Use all projects instead of only WC-projects (#9)
    * BUILDENV: Use all projects instead of only WC-projects
    
    * BUILDENV: Removed monitoring and srs from ALL_PROJECTS

---
+++ buildTools/__projects.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ALL_PROJECTS="common \
+infra \
+intygsadmin \
+intygsbestallning \
+intygstjanst \
+logsender \
+minaintyg \
+privatlakarportal \
+refdata \
+rehabstod \
+schemas \
+statistik \
+webcert"
+
+WC_PROJECTS="common \
+infra \
+intygstjanst \
+minaintyg \
+refdata \
+schemas \
+webcert"
+
--- buildTools/checkStatusAll.sh
+++ buildTools/checkStatusAll.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Get project names
+. __projects.sh
+
 INTYG_HOME="${INTYG_HOME:-$( cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd )}"
 
-for project in schemas common infra intygstjanst minaintyg webcert; do
+for project in $ALL_PROJECTS 
+do
     echo $project
     cd "$INTYG_HOME/$project"
     git status -sb
